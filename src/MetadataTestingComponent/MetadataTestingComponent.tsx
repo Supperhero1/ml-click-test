c3ce371ed698879dfa6e72ebc98a07bc0122987e
Marko Petric <marko.petric@rimac-automobili.com>
2021-07-30 09:54:16 +0200

    try out analytics.js

--- src/MetadataTestingComponent/MetadataTestingComponent.tsx
+++ src/MetadataTestingComponent/MetadataTestingComponent.tsx
@@ -7,8 +7,8 @@ const MetadataTestingComponent = () => {
 
     return <div className="testComponent" id="metadataTestingComponent" >
         <h2>Metadata test</h2>
-        <div id="fixedMetadataComponent" className="testClickBox" data-test={JSON.stringify({test1: 123, test2: "123"})}>FIXED METADATA</div>
-        <div className="testClickBox" data-timedMetadata={JSON.stringify(service.timedMetadata)}>TIMED METADATA TEST</div>
+        <div id="fixedMetadataComponent" onClick={()=>ga("send",{hitType: "event", eventCategory: "eventCategory", eventAction: "eventActioN", eventLabel: "eventLabel", testValue: 123})} className="testClickBox" data-test={JSON.stringify({test1: 123, test2: "123"})}>FIXED METADATA</div>
+        <div className="testClickBox" data-timedmetadata={JSON.stringify(service.timedMetadata)}>TIMED METADATA TEST</div>
     </div>
 }
 

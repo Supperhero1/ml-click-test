e2b7af14bfb6cdfbd730941554629b91a55199d8
Marko Petric <marko.petric@rimac-automobili.com>
2021-07-30 11:45:47 +0200

    forgot to push

--- src/MetadataTestingComponent/MetadataTestingComponent.tsx
+++ src/MetadataTestingComponent/MetadataTestingComponent.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import useMetadataTestingComponent from "./useMetadataTestingComponent";
 import "./MetadataTestingComponent.css"
 
+const onClick = ()=>{
+    (window as any).dataLayer = (window as any).dataLayer || [];
+    (window as any).dataLayer.push({'event': 'clickOnAd'});
+}
+
 const MetadataTestingComponent = () => {
     const service = useMetadataTestingComponent()
 
     return <div className="testComponent" id="metadataTestingComponent" >
         <h2>Metadata test</h2>
-        <div id="fixedMetadataComponent" className="testClickBox" data-test={JSON.stringify({test1: 123, test2: "123"})}>FIXED METADATA</div>
+        <div id="fixedMetadataComponent" className="testClickBox" onClick={onClick}>FIXED METADATA</div>
         <div className="testClickBox" data-timedMetadata={JSON.stringify(service.timedMetadata)}>TIMED METADATA TEST</div>
     </div>
 }
